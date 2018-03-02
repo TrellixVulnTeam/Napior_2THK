0cced1c39e9a1bc43a6ea03ef56332e94a36c9e9
EC2 Default User <ec2-user@ip-172-31-4-118.us-west-2.compute.internal>
2018-03-02 00:52:47 +0000

    deleted unneeded files

--- Client-Napior/run_napior.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-trap 'kill %1' SIGINT
-ng serve --host 0.0.0.0 --port 8080 --aot & cd ../Server-Napior && python3 manage.py runserver 0.0.0.0:8081
--- Server-Napior/refactorSnow.py
+++
@@ -1,6 +0,0 @@
-from napior_api.calculations.modules.snow.Snow_Loads import Snow_Loads
-
-calc = Snow_Loads()
-calc.main()
-calcOutputs = calc.outputs
-print(calcOutputs)
