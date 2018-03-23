218c19d9598afe65ace11b1c74366e0bdeda537c
EC2 Default User <ec2-user@ip-172-31-4-118.us-west-2.compute.internal>
2018-03-23 00:01:53 +0000

    Section accordion added

--- Client-Napior/src/app/screens/calc-modules/beams/common-beam-components/beam-sections-materials/beam-sections-materials.component.ts
+++ Client-Napior/src/app/screens/calc-modules/beams/common-beam-components/beam-sections-materials/beam-sections-materials.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BeamSection } from './beam-section';
 
 @Component({
   selector: 'app-beam-sections-materials',
@@ -6,10 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./beam-sections-materials.component.css']
 })
 export class BeamSectionsMaterialsComponent implements OnInit {
+  
+  public sections = [];
 
-  constructor() { }
+  constructor() {
+
+  }
+  
+  addSection():void {
+    this.sections.push(this.sections[this.sections.length - 1]);
+  }
+  
+  deleteSection(i) {
+    this.sections.splice(i, 1);
+    console.log(this.sections);
+  }
 
   ngOnInit() {
+    const section1 = new BeamSection(
+      '2x4',
+      '2X4',
+      {},
+      'dflno2',
+      'No. 2 DF-L',
+      {}
+    )
+    const section2 = new BeamSection(
+      'gl4x15',
+      'GL 4X15',
+      {},
+      '24fv4',
+      '24F - V4',
+      {}
+    )
+    this.sections = [section1, section2];
+    console.log(this.sections)
   }
 
 }
