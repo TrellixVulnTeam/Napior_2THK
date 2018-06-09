55037fab8e0ff9f8e9fab8c889e55225b2c771f7
Charlie Misner <charliemisner@gmail.com>
2018-06-09 11:52:51 -0700

    Removed problem specs

--- Client-Napior/src/app/screens/calc-modules/general/inputs/napior-input.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NapiorInputDirective } from './napior-input.directive';
-
-describe('NapiorInputDirective', () => {
-  it('should create an instance', () => {
-    const directive = new NapiorInputDirective();
-    expect(directive).toBeTruthy();
-  });
-});
